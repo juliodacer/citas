a3ea070b022172e0cceca9955dd6cde6af9037b2
Julio Davila <juliodacer@gmail.com>
2023-03-02 01:40:39 -0500

    eliminar cita

--- App.js
+++ App.js
@@ -4,32 +4,24 @@ import Citas from './components/Citas'
 
 const App = () => {
   const [citas, setCitas] = useState([
-    {
-      id: '1',
-      paciente: 'Hook',
-      propietario: 'Juan',
-      sintomas: 'No come',
-    },
-    {
-      id: '2',
-      paciente: 'Redux',
-      propietario: 'Itzel', 
-      sintomas: 'No come',
-    },
-    {
-      id: '3',
-      paciente: 'Native',
-      propietario: 'Josue',
-      sintomas: 'No come',
-    },
+    {id: '1', paciente: 'Hook', propietario: 'Juan', sintomas: 'No come'},
+    {id: '2', paciente: 'Redux', propietario: 'Itzel', sintomas: 'No come'},
+    {id: '3', paciente: 'Native', propietario: 'Josue', sintomas: 'No come'}
   ]);
+
+  const eliminarPaciente = id => {
+    setCitas( (pacientes) => { 
+      return pacientes.filter(paciente => paciente.id !== id)
+    })
+  }
 
   return (
     <View style={styles.contenedor}>
       <Text style={styles.titulo}>Administrador de citas</Text>
+      <Text style={[styles.titulo, {marginVertical: 20}]}>{citas.length > 0 ? 'Administrador de citas' : 'Vacio'}</Text>
       <FlatList
         data={citas}
-        renderItem={ ({item}) => <Citas item={item} />
+        renderItem={ ({item}) => <Citas item={item} eliminarPaciente={eliminarPaciente} />
         } // Apariencia que tendra la informacion cuando se muestre
         keyExtractor={ cita => cita.id}
       />
--- components/Citas.js
+++ components/Citas.js
@@ -1,23 +1,65 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native'
 
-const Citas = ({item}) => {
-  return (
-    <View>
+const Citas = ({item, eliminarPaciente}) => {
+  
+    const dialogoEliminar = (id) => {
+        console.log('Eliminando...' + id)
+        eliminarPaciente(id)
+    }
+
+    return (
+    <View style={styles.cita}>
         <View>
-            <Text>Paciente: </Text>
-            <Text>{item.paciente}</Text>
+            <Text style={styles.label}>Paciente: </Text>
+            <Text style={styles.texto}>{item.paciente}</Text>
         </View>
         <View>
-            <Text>Propietario: </Text>
-            <Text>{item.propietario}</Text>
+            <Text style={styles.label}>Propietario: </Text>
+            <Text style={styles.texto}>{item.propietario}</Text>
         </View>
         <View>
-            <Text>Sintomas: </Text>
-            <Text>{item.sintomas}</Text>
+            <Text style={styles.label}>Sintomas: </Text>
+            <Text style={styles.texto}>{item.sintomas}</Text>
+        </View>
+        <View>
+            <TouchableHighlight 
+                onPress={() => dialogoEliminar(item.id)}
+                style={styles.btnEliminar}>
+                    <Text style={styles.txtEliminar}>Eliminar &times;</Text>
+            </TouchableHighlight>
         </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    cita: {
+        backgroundColor: '#fff',
+        borderBottomColor: '#e1e1e1e1',
+        borderStyle: 'solid',
+        borderBottomWidth: 1,
+        paddingVertical: 20,
+        paddingHorizontal: 10
+    },
+    label: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginTop: 20
+    },
+    texto: {
+        fontSize: 18
+    },
+    btnEliminar: {
+        padding: 10,
+        backgroundColor: 'red',
+        marginVertical: 10
+    },
+    txtEliminar: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+})
 
 export default Citas
